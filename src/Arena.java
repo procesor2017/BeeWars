5968c5dc5f439d073ca33a16559bcf44c2df4e51
janegermaier <jan.egermaier@tesena.com>
2020-07-23 12:01:47 +0200

    New class and methods

--- src/Arena.java
+++ src/Arena.java
@@ -1,12 +1,19 @@
 public class Arena {
     public static void main(String[] args) {
-        Character character = new Character();
-        CardDeck cardDeck = new CardDeck();
-        cardDeck.addCard("1");
-        cardDeck.addCard("2");
-        System.out.println(cardDeck.cardFinalDeck);
-        String[] a = cardDeck.useCardFromFinalDeck("1");
-        System.out.println(cardDeck.cardFinalDeck);
+        Character player = new Character();
+        Character enemy = new Character();
+        CardDeck playerCardDeck = new CardDeck();
+        playerCardDeck.addCard("1");
+        playerCardDeck.addCard("2");
+        System.out.println(playerCardDeck.cardFinalDeck);
+
+        // Start fight
+        String[] a = playerCardDeck.useCardFromFinalDeck("1");
+        System.out.println(playerCardDeck.cardFinalDeck);
+        System.out.println(player);
         System.out.println(a);
+        player.getResource(a);
+        System.out.println(player.hp);
+
     }
 }
--- src/Character.java
+++ src/Character.java
@@ -1,27 +1,33 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Character {
-    public int hp = 100, mana = 0, gold = 0, soldier = 0, createMana = 1, createSoldier = 1, createGold = 1;
+    public int hp = 100, mana = 100, gold = 100, soldier = 50, createMana = 1, createSoldier = 1, createGold = 1;
 
-    public int getResource(String[] s){
+    public void getResource(String[] s){
         // get string from CardDeck.useCardFromFinalDeck();
         // in format {id,name,hp,mana,gold,soldier,createMana,createSoldier,createGold}
-
-        int[] intResource = Arrays.stream(s).mapToInt(Integer::parseInt).toArray();
-        hp += intResource[2];
-        mana += intResource[3];
-        gold += intResource[4];
-        soldier += intResource[5];
-        createMana += intResource[6];
-        createSoldier += intResource[7];
-        createGold += intResource[8];
-
-        return 1;
+        String[] a = Arrays.copyOfRange(s, 2,9);
+        int[] intResource = Arrays.stream(a).mapToInt(Integer::parseInt).toArray();
+        hp += intResource[0];
+        mana += intResource[1];
+        gold += intResource[2];
+        soldier += intResource[3];
+        createMana += intResource[4];
+        createSoldier += intResource[5];
+        createGold += intResource[6];
     }
 
-
-    public int characterDef(int damage){
-        hp = hp - damage;
-        return hp;
+    public void lostResource(String[] s){
+        // get string from CardDeck.useCardFromFinalDeck();
+        // in format {id,name,hp,mana,gold,soldier,createMana,createSoldier,createGold}
+        // but u want from 9 to 12 {takeHealth,takeMana,takeGold,takeSoldier}
+        String[] a = Arrays.copyOfRange(s, 9,13);
+        int[] intTakeResource = Arrays.stream(a).mapToInt(Integer::parseInt).toArray();
+        hp -= intTakeResource[9];
+        mana -= intTakeResource[10];
+        gold -= intTakeResource[11];
+        soldier -= intTakeResource[12];
     }
 }
